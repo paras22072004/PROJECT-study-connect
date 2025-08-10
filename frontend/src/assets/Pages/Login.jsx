c7793fe75ef300ca8015f2b39c40208b427d31d3
paras22072004 <parasbiltoria22@gmail.com>
2025-08-10 15:24:52 +0530

    Update Login.jsx

--- frontend/src/assets/Pages/Login.jsx
+++ frontend/src/assets/Pages/Login.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
 
-const API = 'http://localhost:5000/api/auth';
+const API = 'https://project-study-connect-backend.onrender.com/api/auth';
 
 export default function Login(){
   const [email,setEmail] = useState('');
